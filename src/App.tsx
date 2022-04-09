6559b09ae6bca9a9fcbcd470d250127408bda2ca
Marcus Aurelius <8272331@gmail.com>
2022-04-09 20:51:40 +0200

    F

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,37 @@
-import React from 'react';
-import './App.css';
+import React, {useCallback, useEffect, useState} from 'react';
+
+interface Props {
+    handler: () => void
+}
+
+const Foo = ({handler}: Props) => {
+
+    useEffect(() => {
+        handler();
+    }, [handler])
+    return (
+        <h1>Hello World!🚀</h1>
+    )
+}
 
 export const App = () => {
-    return <h1>init🚀</h1>;
+
+    const [counter, setCounter] = useState<number>(0);
+
+    useEffect(() => {
+        const int = setInterval(() => {
+            setCounter(prev => prev + 1)
+        }, 1000);
+
+        return () => {
+            clearInterval(int);
+        }
+    }, [])
+
+
+    const handler = useCallback(() => { //Opakowanie handler-a w useCallback sprawia, że funkcja zadeklarowana zostaje jeden raz. Oznacza to, że przy zmianie stanu komponentu-rodzica (=ponownym jego wyrenderowaniu) handler przekazany w propsie nie zostaje uznany za zmieniony, co oznacza, że komponent-dziecko, do którego został przekazany, nie zostanie wyrenderowany na nowo.
+        console.log(counter)
+    }, [])
+
+    return <Foo handler={handler}/>
 }
