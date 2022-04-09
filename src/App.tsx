ac2ff108a883db1db68e1d2ea452c2df1cfea2f9
Marcus Aurelius <8272331@gmail.com>
2022-04-09 20:58:36 +0200

    F

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
-import React from 'react';
-import './App.css';
+import React, {useRef, useState} from 'react';
 
 export const App = () => {
-    return <h1>init🚀</h1>;
+    const randomRef = useRef<number| null>(null);
+    const [random, setRandom] = useState<number | null>(null);
+
+    const handleClick = () => {
+        randomRef.current = random;
+        setRandom(Math.random() * 100);
+    }
+
+    return <div>
+        <h1>{random}</h1>
+        <h2>{randomRef.current}</h2>
+        <button onClick={handleClick}>click!</button>
+    </div>
 }
+
+
+/* *DESCRIPTION:
+W tym przypadku dzięki useRef przechowujemy poprzednią wartość stanu.
+*/
