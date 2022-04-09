4ce4b616e155e4c9ae88344f75c7e622d2408da8
Marcus Aurelius <8272331@gmail.com>
2022-04-09 20:50:03 +0200

    F

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
-import React from 'react';
-import './App.css';
+import React, {useState} from 'react';
+import {lorem} from './dev/string';
+
+const useToggle = (): [boolean, () => void] => {
+    const [toggled, setToggled] = useState<boolean>(false);
+    const clickHandler = () => {
+        setToggled(prev => !prev)
+    }
+    return [toggled, clickHandler]
+}
 
 export const App = () => {
-    return <h1>init🚀</h1>;
-}
+    const [toggled, clickHandler] = useToggle();
+
+    return <>
+        <button onClick={clickHandler}>{toggled ? 'minimize' : 'maximize'}</button>
+        <p style={{
+            height: toggled ? 'auto' : '20px',
+            overflow: 'hidden',
+        }}>{lorem}</p>
+    </>;
+};
